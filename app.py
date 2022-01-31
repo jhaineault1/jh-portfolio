7606f7d2ca625940bc1919e10e58bccf30502fff
jhaineault <jhaineault1@gmail.com>
2022-01-30 19:28:23 -0800

    Added projects page and sqlalchemy

--- app.py
+++ app.py
@@ -1,6 +1,19 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///friends.db'
+# Initialize db
+db = SQLAlchemy(app)
+#Create db model
+class Friends(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(200), nullable=False)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+#Create function to return string when we add something
+    def __repr___(self):
+        return '<Name %r>' % self.id
 
 #flask route for each tab
 @app.route('/')
